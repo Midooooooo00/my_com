7adc60f4ce124f92d5bf89c091b4e2c58d4401e4
M <muhammed_safaa@fcom.bu.edu.eg>
2022-10-16 13:31:17 +0200

    Initial commit

---
+++ lib/Screens/HomeScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeScreens extends StatelessWidget {
+  static var RoutName = "HomeScreen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:my_com/Screens/HomeScreen.dart';
+
+class My_Com extends StatelessWidget {
+  const My_Com({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: "HomeScreen",
+        routes: {
+          "HomeScreen": (_) => HomeScreens(),
+        }
+    );
+  }
+}
+main(){
+  runApp(My_Com());
+}
